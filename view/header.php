fed09d4ab887054ce1a9bca574f8ad6c2ba346f7
tam986 <39400292+tam986@users.noreply.github.com>
2024-07-26 18:40:27 +0700

    xong template home

---
+++ view/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./asset/style.css">
+  <script src="https://kit.fontawesome.com/b7874c4c11.js" crossorigin="anonymous"></script>
+  <!-- font family -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+  <!-- end font -->
+</head>
+
+<body>
+  <section class="placeMaster">
+    <!-- header -->
+    <header>
+      <div class="place-header">
+        <div class="user">
+          <div class="info">
+            <img src="./asset/img/user3.png" alt="">
+            <span>Name</span>
+          </div>
+        </div>
+        <nav>
+          <ul>
+            <li><a href=""><i class="fa-solid fa-house"></i></a></li>
+            <li><a href=""><i class="fab fa-product-hunt"></i></a></li>
+            <li><a href=""><i class="fab fa-yarn"></i></a></li>
+            <li><a href="">Blogs</a></li>
+          </ul>
+        </nav>
+        <div class="login-logout">
+          <a href=""><i class="fas fa-sign-out-alt"></i></a>
+        </div>
+      </div>
+    </header>
+    <!-- end header -->
---
+++ view/home.php
@@ -0,0 +1,286 @@
+  <!-- main -->
+  <main class="home">
+    <section class="place-main">
+      <!-- header main -->
+      <div class="content">
+        <div class="header">
+          <div class="logo">
+            <a href=""><img src="./asset/img/logo.png" alt=""></a>
+
+          </div>
+          <div class="textheader">
+            <div class="search-container">
+              <input type="text" class="search-input" placeholder="Tìm kiếm...">
+              <button class="search-button">
+                <i class="fas fa-search"></i>
+              </button>
+            </div>
+            <div class="cart">
+              <a href=""><i class="fas fa-cart-plus"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end header main -->
+      <!-- banner -->
+      <div class="content">
+        <div class="box-banner">
+          <a href=""><img src="./asset/img/banner.png" alt=""></a>
+
+
+          <div class="box-text-banner">
+            <h1>Hi!</h1>
+            <h1>Tiệm Len Ấm Xin Chào</h1>
+            <a href="">Mời bạn trải nghiệm tại đây </a>
+          </div>
+
+
+        </div>
+
+      </div>
+      <!-- end banner -->
+      <!-- danh mục tổng -->
+      <div class="content">
+        <div class="place-main-home1">
+          <div class="yarn">
+            <div class="banner-yarn">
+              <a href="">
+                <div class="image-banner">
+                  <img src="./asset/img/banner2.png" alt="">
+                </div>
+              </a>
+
+              <div class="text-banner-yarn">
+                <h2><a href="">Trang phục và Phụ kiện từ len</a></h2>
+                <span>Đa dạng trang phục phụ kiện được làm từ Len đang chờ bạn khám phá.</span>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="pattern">
+            <div class="banner-pattern">
+              <a href="">
+                <div class="image-banner">
+                  <img src="./asset/img/banner3.png" alt="">
+                </div>
+              </a>
+
+              <div class="text-banner-yarn">
+                <h2><a href="">Tất cả loại len và dụng cụ đan len</a></h2>
+                <span>Đa dạng cuộn len và các dụng cụ đang chờ bạn khám phá.</span>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+      <!-- end danh mục tổng -->
+      <!-- start banner 3 -->
+      <div class="content">
+        <div class="box-banner2">
+          <a href=""><img src="./asset/img/banner4.png" alt=""></a>
+
+          <div class="box-text-banner">
+            <h1>Voucher đầy kho</h1>
+            <a href="">Trải nghiệm ngay nào </a>
+          </div>
+
+        </div>
+      </div>
+      <!-- end banner 3 -->
+      <!-- catagories -->
+      <div class="content">
+        <div class="place-danhmuc">
+          <div class="h3-danhmuc">
+            <h3>DANH MỤC SẢN PHẨM</h3>
+          </div>
+          <div class="box-danhmuc">
+            <div class="box-danhmuc1">
+              <img src="./asset/img/banner3.png" alt="">
+              <a href="">
+                <h1>Trang phục và Phụ kiện</h1>
+              </a>
+            </div>
+            <div class="box-danhmuc1">
+              <img src="./asset/img/banner2.png" alt="">
+              <a href="">
+                <h1>Cuộn len và Dụng cụ đan</h1>
+              </a>
+            </div>
+            <div class="box-danhmuc1">
+              <img src="./asset/img/product3-1.png" alt="">
+              <a href="">
+                <h1>Tips and trick</h1>
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <!-- end categories -->
+      <!-- start place pattern hot -->
+      <div class="content">
+        <div class="place-product">
+          <div class="heading-place-product">
+            <div class="heading">
+              <h1><a href="">Trang Phục và phụ kiện Hot</a></h1>
+            </div>
+            <div class="more">
+              <h1><a href="">Xem Thêm</a></h1>
+            </div>
+          </div>
+          <div class="box-product-master">
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern2.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Nón</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern1.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Nón</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern3.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Nón</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern3.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Nón</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end pattern hot -->
+      <!-- start len hot -->
+      <div class="content">
+        <div class="place-product">
+          <div class="heading-place-product">
+            <div class="heading">
+              <h1><a href="">Len bán chạy</a></h1>
+            </div>
+            <div class="more">
+              <h1><a href="">Xem Thêm</a></h1>
+            </div>
+          </div>
+          <div class="box-product-master">
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern2.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Len 1</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern1.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Len 2</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern3.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Len 3</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+            <div class="box-product">
+              <span class="sale-off-tag">Sale Off</span>
+              <img src="./asset/img/pattern3.jpg" alt="">
+              <div class="content-box-product">
+                <h1><strong>Name</strong></h1>
+                <h2>Len 3</h2>
+                <h3>300.000 VND</h3>
+              </div>
+              <button class="buy-now-button">Mua Ngay</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- end len hot -->
+      <!-- start banner 3 -->
+      <div class="content">
+        <div class="box-banner3">
+          <a href=""><img src="./asset/img/banner4.png" alt=""></a>
+
+
+          <div class="box-text-banner">
+            <h1>Hi!</h1>
+            <a href="">Mời Bạn Trải Nghiệm Tại Đây </a>
+          </div>
+
+
+        </div>
+      </div>
+      <!-- end banner 3 -->
+      <div class="footer--main">
+        <div class="content">
+          <div class="loiich">
+            <div class="loiich1">
+              <a href="">
+                <i class="fas fa-shipping-fast"></i>
+                <span>Giao Hàng và Đổi trả</span>
+              </a>
+
+            </div>
+            <div class="loiich2">
+              <a href="">
+                <i class="fas fa-comments"></i>
+                <span>Liên Lạc với chúng tôi</span>
+              </a>
+
+            </div>
+            <div class="loiich3">
+              <a href="">
+                <i class="fas fa-map-marker"></i>
+                <span>Địa chỉ cửa hàng</span>
+              </a>
+
+            </div>
+            <div class="loiich4">
+              <a href="">
+                <i class="fas fa-gift"></i>
+                <span>Voucher</span>
+              </a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <!-- end main -->
